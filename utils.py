e7726fb42ba0db4b231874afb81984e294f7aad3
Plyshk1n2k <plyshk1n2k@gmail.com>
2024-06-30 14:07:07 +0500

    FIX: - Добавлены принты этапов работы утилиты

--- utils.py
+++ utils.py
@@ -11,19 +11,27 @@ async def background_task():
         await logger.log('Запускаем фоновую задачу...', level=LogLevel.INFO)
         async with MoyskladAPI() as api:
             while True:
+                print('Получаем данные о клиентах')
                 await api.get_counterparties(tag="клиент")
                 await asyncio.sleep(60)
+                print('Получаем данные о магазинах')
                 await api.get_stores()
                 await asyncio.sleep(60)
+                print('Получаем данные о единицах измерения')
                 await api.get_uoms()
                 await asyncio.sleep(60)
+                print('Получаем данные о группах товаров')
                 await api.get_groups_product()
                 await asyncio.sleep(60)
+                print('Получаем данные о товарах')
                 await api.get_products()
                 await asyncio.sleep(60)
+                print('Получаем данные об остатках на складах')
                 await api.get_stock_by_stores()
                 await asyncio.sleep(60)
+                print('Получаем данные о бонусных операциях')
                 await api.get_bonus_operations()
+                print('Ожидаем следующего цикла')
                 await asyncio.sleep(300)  # Пример: ждем 300 секунд между итерациями
     except asyncio.CancelledError:
         await logger.log('Background task is cancelled.', level=LogLevel.INFO)
